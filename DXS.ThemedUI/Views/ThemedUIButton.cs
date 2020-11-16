381af7d9dcefd38a97feb2c5f3260eecee8dd2dc
Patric Cunha <patricc@agap2.pt>
2020-11-16 16:04:01 +0000

    Updated ThemedUIButton to add UIControlState customization

--- DXS.ThemedUI/Views/ThemedUIButton.cs
+++ DXS.ThemedUI/Views/ThemedUIButton.cs
@@ -11,6 +11,32 @@ namespace DXS.ThemedUI.Views
     [Register("ThemedUIButton"), DesignTimeVisible(true)]
     public class ThemedUIButton : UIButton
     {
+        public UIColor NormalBackgroundColor { get; set; }
+        public UIColor HighlightedBackgroundColor { get; set; }
+        public UIColor DisabledBackgroundColor { get; set; }
+
+        public override bool Enabled
+        {
+            get { return base.Enabled; }
+            set
+            {
+                base.Enabled = value;
+                var state = value ? UIControlState.Normal : UIControlState.Disabled;
+                SetBackgroundColorForState(state);
+            }
+        }
+
+        public override bool Highlighted
+        {
+            get { return base.Highlighted; }
+            set
+            {
+                base.Highlighted = value;
+                var state = value ? UIControlState.Highlighted : UIControlState.Normal;
+                SetBackgroundColorForState(state);
+            }
+        }
+
         public ThemedUIButton() : base()
         {
             Initialize();
@@ -39,6 +65,31 @@ namespace DXS.ThemedUI.Views
         void Initialize()
         {
             this.WithStyle(ThemedUI.CurrentTheme.ThemedUIButtonStyle);
+        }
+
+        protected void SetBackgroundColorForState(UIControlState state)
+        {
+            switch (state)
+            {
+                case UIControlState.Normal:
+                    if (NormalBackgroundColor != null)
+                    {
+                        BackgroundColor = NormalBackgroundColor;
+                    }
+                    break;
+                case UIControlState.Highlighted:
+                    if (HighlightedBackgroundColor != null)
+                    {
+                        BackgroundColor = HighlightedBackgroundColor;
+                    }
+                    break;
+                case UIControlState.Disabled:
+                    if (DisabledBackgroundColor != null)
+                    {
+                        BackgroundColor = DisabledBackgroundColor;
+                    }
+                    break;
+            }
         }
     }
 }
